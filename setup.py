e528ff77059e5d929be7a99d9ba44701c9345750
xlepotato <yucia181@gmail.com>
2025-01-06 00:39:25 +0800

    refactor: format code

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import find_packages, setup
 from typing import List
 
+
 HYPEN_E_DOT = '-e .'
+
 def get_requirements(file_path:str) -> List[str]:
     """Return the list of requirements."""
     requirements = []
@@ -11,6 +13,7 @@ def get_requirements(file_path:str) -> List[str]:
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
     return requirements
+
 
 setup(
     name='visa-approval-prediction',
